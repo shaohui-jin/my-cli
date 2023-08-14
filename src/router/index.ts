cc6aa972886cb3602c12c34c462640b095e92cc0
shaohui_jin <1051131737@qq.com>
2023-08-14 15:13:56 +0800

    feat(全局调整): 配置文件调整
    调整成不同环境配置文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,7 @@ const defaultRoute: Route[] = [
   ...routes
 ]
 
+console.log(import.meta.env.VITE_APP_BASE_PATH)
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_BASE_PATH), //路由模式的配置采用API调用的方式 不再是之前的字符串 此处采用的hash路由
   routes: defaultRoute
